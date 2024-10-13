8c0b4acd0779c40050d2b164c2984bc87ed5131d
Chiragm550 <113295581+Chiragm550@users.noreply.github.com>
2024-10-13 18:02:53 +0530

    Time: 6 ms (88.50%) | Memory: 14.6 MB (14.69%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        vector<int> indices;
+        int idx = 0;
+        map<int, int> mpp;
+
+        while(idx < nums.size()){
+            int second = target - nums[idx];
+            if(mpp.find(second) != mpp.end()){
+                indices.push_back(idx);
+                indices.push_back(mpp[second]);
+                break;
+            }
+            else{
+                mpp[nums[idx]] = idx;
+            }
+            idx++;
+        }
+        return indices;
+    }
+};
