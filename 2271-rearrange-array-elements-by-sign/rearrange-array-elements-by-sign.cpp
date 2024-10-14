567f11e048bcc6c55d578f04365be80f2b095271
Chiragm550 <113295581+Chiragm550@users.noreply.github.com>
2024-10-14 16:50:33 +0530

    Time: 134 ms (80.81%) | Memory: 126.7 MB (54.14%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>pos;
-        vector<int>neg;
-        vector<int>final;
-        int n = nums.size();
-        for(int i=0; i<n; i++){
-            if(nums[i]<0)
-                neg.push_back(nums[i]);
-            else
-                pos.push_back(nums[i]);
-        }
+        // 1 SOLUTION
+        // vector<int>pos;
+        // vector<int>neg;
+        // vector<int>final;
+        // int n = nums.size();
+        // for(int i=0; i<n; i++){
+        //     if(nums[i]<0)
+        //         neg.push_back(nums[i]);
+        //     else
+        //         pos.push_back(nums[i]);
+        // }
 
-        for(int i=0; i<n/2; i++){
-            final.push_back(pos[i]);
-            final.push_back(neg[i]);
+        // for(int i=0; i<n/2; i++){
+        //     final.push_back(pos[i]);
+        //     final.push_back(neg[i]);
+        // }
+
+        // return final
+
+        // 2 SOLUTION
+        vector<int>numsSorted(nums.size());
+        int neg = -1;
+        int pos = -2;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] < 0){
+                neg += 2;
+                numsSorted[neg] = nums[i];
+            }
+            else{
+                pos += 2;
+                numsSorted[pos] = nums[i];
+            }
         }
-        return final;
+        return numsSorted;
     }
 };
