f5a78402002596ad7735df2331300b56d69c5872
Chiragm550 <113295581+Chiragm550@users.noreply.github.com>
2024-10-15 15:42:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (10.65%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -27,24 +27,19 @@ public:
                     total++;
                     j++;
                 }
-                // cout << endl << i << ' ' << j << ' ' << m << ' ' << n << endl;
             }
             else if(i==m && j==n){
                 int k = i-2;
                 i = i - 1;
-                // cout << endl << i << ' ' << j << ' ' << k <<  endl;
                 for(; k >= inside; k--){
-                    // cout << endl << i << ' ' << j << ' ' << k << endl;
                     v.push_back(matrix[j-1][k]);
                     total++;
                     i--;
                 }
-                // cout << endl << i << ' ' << j << total << endl;
             }
             else if(i==inside && j==n){
                 int k = j-2;
                 j -= 1;
-                // cout << endl << i << ' ' << j << endl;
                 for(; k>inside; k--){
                     v.push_back(matrix[k][i]);
                     total++;
