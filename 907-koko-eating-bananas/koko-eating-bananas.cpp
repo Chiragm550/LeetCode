59076f8867718557286559c0364908c4dd57af06
Chiragm550 <113295581+Chiragm550@users.noreply.github.com>
2024-10-28 19:56:47 +0530

    Time: 31 ms (5.57%) | Memory: 21.6 MB (78.18%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int max_pile = *max_element(piles.begin(), piles.end());
+        int min_k = 0;
+
+        int low = 1, high = max_pile;
+        // cout << max_pile << endl;
+        while(low <= high){
+            int mid = (low+high)/2;
+            long time = 0;
+            for(int i=0; i<n; i++){
+                time += ceil(double(double(piles[i])/double(mid)));
+                
+                // cout << mid << " " << ceil(double(double(piles[i])/double(mid))) << endl;
+            }
+            if(time <= h){
+                cout << mid << " " << time << endl;
+                min_k = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return min_k;
+    }
+};
